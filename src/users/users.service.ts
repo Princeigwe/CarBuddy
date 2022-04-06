c356c2b8ca3b5d0525e015abc1ae2159d00ce881
Prince Igwe <igwep297@gmail.com>
2022-04-06 22:54:56 +0100

    added logic to find, findOne, updateuser service function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,11 +6,43 @@ import {User} from 'src/users/user.entity'
 @Injectable()
 export class UsersService {
     constructor(
-      @InjectRepository(User)  private repo: Repository<User> // inject the User Repository that was defined by TypeORM
+      @InjectRepository(User)  private repo: Repository<User> // create repository for User Entity, and inject it as a dependency in the service.
     ){}
 
     create(email:string, password:string){
         const user = this.repo.create({email, password}); // create a user instance in the User Repository
         return this.repo.save(user) // save the user instance
+    }
+
+    // get user instance with an ID
+    findOne( id: number ) {
+      return this.findOne(id)
+    }
+
+    // get all user instances by email that was given
+    find(email: string) {
+      return this.repo.find({ email})
+    }
+
+    //update one or more attributes of the user instance, with the ID provided
+    async update( id: number, attrs: Partial<User>) {
+      const user = await this.findOne(id)
+      // if there was no user with this ID, throw an error
+      if (!user) { 
+        throw new Error(`User with ${id} does not exist`)
+      }
+      // if user was found, make an update
+      Object.assign(user, attrs) // make the update
+      return this.repo.save(user) // save the update
+    }
+
+    // remove a user instance
+    async remove(id: number) {
+      const user = await this.findOne(id)
+      // if user is not found, throw error
+      if (!user){
+        throw new Error(`User ${id} does not exist`)
+      }
+      return this.repo.remove(user) // remove the user entity
     }
 }
