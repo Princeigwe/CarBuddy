1a2a64746866509a244b767595d4364b33b98f07
Prince Igwe <igwep297@gmail.com>
2022-05-25 22:47:41 +0100

    added add to cart feature

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -29,49 +29,113 @@ export class CartService {
     }
 
     async addToCart(cartOwnerEmail: string, id: number, quantity: number) {
+
         const car = await this.carsService.getPublicCarByIdForCart(id)
         const totalPrice = car.estimatedPrice * quantity
 
-        const product = {
-            style: car.style,
-            releaseYear: car.releaseYear,
-            brand: car.brand,
-            model: car.model,
-            useType: car.useType,
-            estimatedPrice: car.estimatedPrice,
-            mileage: car.mileage,
-            location: car.location,
-            exteriorColour: car.exteriorColour,
-            interiorColour: car.interiorColour,
-            milesPerGallon: car.milesPerGallon,
-            engine: car.engine,
-            driveType: car.driveType,
-            fuelType: car.fuelType,
-            transmissionType: car.transmissionType,
-            dealer: car.dealer,
-            extraFeature: car.extraFeature,
-            quantity: quantity,
-            totalPrice: totalPrice,
+        // TODO: to check if the item to be added is already in the cart
+        //? there's probably a better way to write this
+
+        const itemExists = await this.cartModel.findOne({
+            cartOwnerEmail: cartOwnerEmail, 
+
+            'items.style': car.style,
+            'items.releaseYear': car.releaseYear,
+            'items.brand': car.brand,
+            'items.model': car.model,
+            'items.useType': car.useType,
+            'items.estimatedPrice': car.estimatedPrice,
+            'items.mileage': car.mileage,
+            'items.location': car.location,
+            'items.exteriorColour': car.exteriorColour,
+            'items.interiorColour': car.interiorColour,
+            'items.milesPerGallon': car.milesPerGallon,
+            'items.engine': car.engine,
+            'items.driveType': car.driveType,
+            'items.fuelType': car.fuelType,
+            'items.transmissionType': car.transmissionType,
+            'items.dealer': car.dealer,
+            'items.extraFeature': car.extraFeature,
+
+        })
+
+        // if the item exists in the items list, increment the item quantity and total price, and the cart total Price
+        if (itemExists){
+
+            // the price change that will reflect in an item if it exists already
+            let addNewPrice = car.estimatedPrice * quantity 
+
+            await this.cartModel.updateOne({
+                cartOwnerEmail: cartOwnerEmail, 
+    
+                'items.style': car.style,
+                'items.releaseYear': car.releaseYear,
+                'items.brand': car.brand,
+                'items.model': car.model,
+                'items.useType': car.useType,
+                'items.estimatedPrice': car.estimatedPrice,
+                'items.mileage': car.mileage,
+                'items.location': car.location,
+                'items.exteriorColour': car.exteriorColour,
+                'items.interiorColour': car.interiorColour,
+                'items.milesPerGallon': car.milesPerGallon,
+                'items.engine': car.engine,
+                'items.driveType': car.driveType,
+                'items.fuelType': car.fuelType,
+                'items.transmissionType': car.transmissionType,
+                'items.dealer': car.dealer,
+                'items.extraFeature': car.extraFeature,
+    
+            }, {
+                $inc: { 'items.$[].quantity': quantity, 'items.$[].totalPrice': addNewPrice, finalTotal: addNewPrice }
+            })
         }
 
-        // adding the item to the cart items array
-        await this.cartModel.updateOne( {'cartOwnerEmail': cartOwnerEmail}, { $push: {items: product} })
+        // if the item doesn't exist, add it to the item'
+        else {
 
+            const product = {
+                style: car.style,
+                releaseYear: car.releaseYear,
+                brand: car.brand,
+                model: car.model,
+                useType: car.useType,
+                estimatedPrice: car.estimatedPrice,
+                mileage: car.mileage,
+                location: car.location,
+                exteriorColour: car.exteriorColour,
+                interiorColour: car.interiorColour,
+                milesPerGallon: car.milesPerGallon,
+                engine: car.engine,
+                driveType: car.driveType,
+                fuelType: car.fuelType,
+                transmissionType: car.transmissionType,
+                dealer: car.dealer,
+                extraFeature: car.extraFeature,
+                quantity: quantity,
+                totalPrice: totalPrice,
+            }
+    
+            // adding the item to the cart items array
+            await this.cartModel.updateOne( {'cartOwnerEmail': cartOwnerEmail}, { $push: {items: product} })
+    
+    
+            // returning only value of the items key
+            let cart = await this.cartModel.findOne( { 'cartOwnerEmail': cartOwnerEmail } )
+    
+            let prices: number[] = []
+    
+            for (var item of cart.items) {
+                prices.push(item['totalPrice'])
+            }
+    
+            // summing items in the prices array with TypeScript reduce method
+            let finalTotal = prices.reduce( (accumulate,current) => accumulate + current, 0 ) // cart final Total price
+    
+            // setting finalTotal  field to finalTotal
+            await this.cartModel.updateOne( {'cartOwnerEmail': cartOwnerEmail}, { $set: {finalTotal: finalTotal} })
 
-        // returning only value of the items key
-        let cart = await this.cartModel.findOne( { 'cartOwnerEmail': cartOwnerEmail } )
-
-        let prices: number[] = []
-
-        for (var item of cart.items) {
-            prices.push(item['totalPrice'])
         }
-
-        // summing items in the prices array with TypeScript reduce method
-        let finalTotal = prices.reduce( (accumulate,current) => accumulate + current, 0 ) // cart final Total price
-
-        // setting finalTotal  field to finalTotal
-        await this.cartModel.updateOne( {'cartOwnerEmail': cartOwnerEmail}, { $set: {finalTotal: finalTotal} })
 
     }
 
