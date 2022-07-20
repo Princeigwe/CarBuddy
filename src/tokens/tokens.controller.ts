f35587004e6b23726f5dbaed571888446bbabb73
Prince Igwe <igwep297@gmail.com>
2022-07-20 03:41:18 +0100

    session token viewing in response when calling the response

--- src/tokens/tokens.controller.ts
+++ src/tokens/tokens.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Post, Body, Get, Delete, Request, Session, Redirect } from '@nestjs/common';
+import { Controller, Post, Body, Get, Delete, Request, Session, Redirect, Render } from '@nestjs/common';
 import { TokensService } from './tokens.service';
 import { ForgotPasswordEmailDto } from './dtos/forgotPasswordEmail';
 import { request } from 'https';
 import {ConfirmPasswordResetDto} from './dtos/confirmPasswordReset';
+import {OnEvent} from '@nestjs/event-emitter'
+import {PasswordTokenCreatedEvent} from '../events/token.created.event'
+
 
 @Controller('tokens')
 export class TokensController {
@@ -11,10 +14,11 @@ export class TokensController {
 
     @Post('forgot-password')
     async forgotPasswordEmail ( @Body() body: ForgotPasswordEmailDto, @Request() request) {
-        var forgotPasswordEmail = this.tokensService.forgotPasswordEmail(body.email)
+        var forgotPasswordEmail = await this.tokensService.forgotPasswordEmail(body.email)
 
         // setting a session object
-        request.session.passwordResetToken = { passwordResetToken : (await forgotPasswordEmail).token.tokenString }
+        request.session.passwordResetToken =  forgotPasswordEmail.token.tokenString 
+        request.session.save()
         console.log(request.session.passwordResetToken)
 
         // returning response of the forgotPasswordEmail method
@@ -22,8 +26,13 @@ export class TokensController {
 
     }
 
+    @Get('password-reset/:tokenString')
+    async sessionToken(@Request() request) {
+        return { tokenString: request.session.passwordResetToken}
+    }
+
     // after password reset, redirect to public cars endpoint
-    @Post('confirm-password-reset')
+    @Post('confirm-password-reset/:token')
     @Redirect('http://localhost:3000/cars/public') // change url to production url in production environment
     async confirmPasswordReset ( @Body() body: ConfirmPasswordResetDto, @Request() request) {
         // getting the token from the session, and use it in the body data
